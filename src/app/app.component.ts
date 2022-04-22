9698cde2d9d98ee77208f97cb224857c86521293
VasikaranR <vasichan2000@gmail.com>
2022-04-22 14:41:05 +0530

    form

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User} from './user'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tdfForm';
+  topics =['angular','react','php'];
+  userModel=new User('','18euee065@skcet.ac.in',635241789,'');
+  onSubmit()
+  {
+    console.log(this.userModel);
+  }
+}
